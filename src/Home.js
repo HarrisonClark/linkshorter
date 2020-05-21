b6dacffebea56be31f6442f17f45172e2c726793
Hana Kontrec <hk5gc@gmail.com>
2020-05-21 09:26:28 -0400

    local changes

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'; 
 import { Box } from "@material-ui/core";
 import LinkForm from './LinkForm';
+import NavBar from './NavBar'; 
 
 export default function Home(){
     return(
         <>
+        <NavBar />
+
         <Box display="flex" justifyContent="center">
             <LinkForm />
         </Box>
