511014883387ec56f2aca5ba18d245a6cf4d7c94
Hana Kontrec <hk5gc@gmail.com>
2020-05-21 09:13:40 -0400

    worked on Analytics Page

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,8 +1,58 @@
 import React from 'react'; 
+import NavBar from './NavBar'
+import { Box,
+        Grid,
+        Paper,
+        makeStyles } from "@material-ui/core";
+
+import GoToForm from './GoToNewLinkForm'
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
 
 export default function Analytics(){
-
+    const classes = useStyles();
+    
     return(
-        <h1> Welcome to the Analytics Page</h1>
+        <>
+        <NavBar />
+
+        <div className={classes.root}>
+            <Grid container spacing={1}>
+                
+                <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                    <Box display="flex" justifyContent="center">
+                        <div>
+                            <h1> Go to your new link: </h1>
+                            <GoToForm />
+                        </div>
+                    </Box>
+                </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                    <Box display="flex" justifyContent="center">
+                        <div>
+                            <h1> View Analytics of Your New Link: </h1>
+                            
+                        </div>
+                    </Box>
+                </Paper>
+                </Grid>
+    
+            </Grid>
+        </div>
+        </>
+        
     );
 }
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from './Home'
 import Analytics from './Analytics'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from './NavBar'
 
 export default function App() {
   return (
