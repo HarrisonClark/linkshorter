6ab50e873da7369986e69033ea26a815e0b6fa7e
Hana Kontrec <hk5gc@gmail.com>
2020-05-21 09:34:02 -0400

    local changes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ export default function App() {
   return (
     <Router>
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/" component={Home} />
-        <Route exact path="/analytics" component={Analytics} />
-=======
         <Route exact path='/' component={Home}/>
         <Route exact path='/analytics' component={Analytics}/>
->>>>>>> 5f736d9412c1b54a3328b3f65789db96c47fdc1a
       </Switch>
     </Router>
   );
